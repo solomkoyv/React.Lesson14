69d3041bfd44759e1f1d3e2e1092621da91ecf8d
Drive-Mac <Drive-Mac@Drives-MacBook.local>
2019-07-01 22:44:08 +0300

    ДЗ урок 16

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -21,35 +21,6 @@ const reducer = (state = initialState, action) => {
         loading: true
       };
 
-    // case "ITEM_ADD_TO_CART":
-    //   const id = action.payload;
-    //   const item = state.menu.find(item => item.id === id);
-    //   const newItem = {
-    //     title: item.title,
-    //     price: item.price,
-    //     url: item.url,
-    //     id: item.id,
-    //     counter: 0
-    //   };
-    //   const double = state.items.some(item => item.id === id);
-    //   if (double) {
-    //     state.items = state.items.map(item => {
-    //       if (item.id === id) item.counter++;
-    //       const itemIndex = state.items.findIndex(item => item.id === id);
-    //       return {
-    //         ...state,
-    //         items: [
-    //           ...state.items.slice(0, itemIndex),
-    //           ...state.items.slice(itemIndex + 1)
-    //         ]
-    //       };
-    //     });
-    //   }
-    //   return {
-    //     ...state,
-    //     items: [...state.items, newItem]
-    //   };
-
     case "ITEM_ADD_TO_CART":
       const id = action.payload;
       const item = state.menu.find(item => item.id === id);
@@ -58,34 +29,40 @@ const reducer = (state = initialState, action) => {
         price: item.price,
         url: item.url,
         id: item.id,
-        counter: 0
+        counter: 1,
+        total: item.price
       };
-      // const double = state.items.find(item => item.id === id);
-      // console.log(double);
-      // if (!double) {
-      //   return {
-      //     ...state,
-      //     items: [...state.items, newItem],
-      //     total: +item.price
-      //   };
-      // } else
-      return {
-        ...state,
-        items: [...state.items, newItem],
-        total: state.total + item.price
-      };
+      const double = state.items.find(item => item.id === id);
+      if (!double) {
+        console.log(item.price);
+        return {
+          ...state,
+          items: [...state.items, newItem],
+          total: state.total + item.price
+        };
+      } else {
+        const items = [...state.items];
+        double.counter++;
+        double.total = double.price * double.counter;
+        return {
+          ...state,
+          items,
+          total: state.total + item.price
+        };
+      }
 
     case "ITEM_REMOVE_FROM_CART":
       const idx = action.payload;
       const itemIndex = state.items.findIndex(item => item.id === idx);
-      const itemX = state.menu.find(item => item.id === idx);
+      const itemX = state.items.find(item => item.id === idx);
+      console.log(itemX);
       return {
         ...state,
         items: [
           ...state.items.slice(0, itemIndex),
           ...state.items.slice(itemIndex + 1)
         ],
-        total: state.total - itemX.price
+        total: state.total - itemX.total
       };
     case "MENU_ERROR":
       return {
