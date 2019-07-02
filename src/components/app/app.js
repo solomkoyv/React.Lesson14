920aa921114f608df71fe4e89b03ffccfbeadb89
Drive-Mac <Drive-Mac@Drives-MacBook.local>
2019-07-02 09:24:34 +0300

    ДЗ урок 16 +  Switch

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,25 +1,25 @@
 import React from "react";
 import { MainPage, CartPage } from "../pages";
 import AppHeader from "../app-header";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 
 import Background from "./food-bg.jpg";
 import "./app.css";
 
 const App = () => {
   return (
-    <Router>
-      <div
-        style={{
-          background: `url(${Background}) center center/cover no-repeat`
-        }}
-        className="app"
-      >
-        <AppHeader total={10} />
+    <div
+      style={{
+        background: `url(${Background}) center center/cover no-repeat`
+      }}
+      className="app"
+    >
+      <AppHeader total={10} />
+      <Switch>
         <Route path="/" exact component={MainPage} />
         <Route path="/cart-page" component={CartPage} />
-      </div>
-    </Router>
+      </Switch>
+    </div>
   );
 };
 
